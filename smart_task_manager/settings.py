80e37cb7fc681c439e1f15edc8b6ec1b078218c7
Hermela Anteneh <hermelaanteneh3@gmail.com>
2025-12-29 18:34:33 +0300

    feat:add email configration for task reminders

--- smart_task_manager/settings.py
+++ smart_task_manager/settings.py
@@ -150,3 +150,12 @@ SIMPLE_JWT = {
 SWAGGER_SETTINGS = {
     'USE_SESSION_AUTH': False,   
 }
+
+# Email settings for task reminders
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Gmail SMTP server
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'        # Replace with your email
+EMAIL_HOST_PASSWORD = 'your_app_password'      # Use Gmail App Password, NOT normal password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
